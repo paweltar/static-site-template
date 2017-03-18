5b0ffdd81d057d65509ed061dd6ac1f9cebb10cc
paweltar <paweltar@gmail.com>
2017-03-18 15:50:32 +0100

    Add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var panini = require('panini');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('serve', function() {
     browserSync.init({
@@ -13,7 +14,9 @@ gulp.task('serve', function() {
 
 gulp.task('sass', function () {
   return gulp.src('src/assets/sass/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/css'));
 });
 
