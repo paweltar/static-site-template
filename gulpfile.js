a916645a1d8a90885808416b6e23a69932a5088c
paweltar <paweltar@gmail.com>
2017-03-18 14:42:38 +0100

    Add panini

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 const chalk = require('chalk');
+var panini = require('panini');
 
 gulp.task('default', function() {
-  console.log(chalk.green.bold('Hello a-static!'));
+  gulp.src('src/pages/**/*.html')
+    .pipe(panini({
+      root: 'src/pages/',
+      layouts: 'src/layouts/',
+      partials: 'src/partials/',
+      helpers: 'src/helpers/',
+      data: 'src/data/'
+    }))
+    .pipe(gulp.dest('dist'));
 });
